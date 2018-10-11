c5835030608a6a07bc6ccaa4de01d2caa0f7d563
jomigo96 <jomigo96@yahoo.com>
2018-10-11 15:00:41 +0100

    dados lab3

--- Week2/Calibracao/regression.R
+++ Week2/Calibracao/regression.R
@@ -1,19 +1,19 @@
-R <- 100 #Ohm
+# R = 100 Ohm
 
 measurements <- data.frame(
 	c(465, 608, 3, 104, 250),
 	c(213, 263, 34, 53, 123),
 	c(268, 328, 51, 88, 169))
 
-names(measurements) <- c("luminosity", "voltage ldr1", "voltage ldr2")
+names(measurements) <- c("luminosity", "voltage-ldr1", "voltage-ldr2")
 # luminosity in LUX, voltages in mV
 
 convert_Ohm <- function(v){
-	return (v*1000-5)/(v*1000/R)
+	return (5-v/1000)/((v/1000)/100)
 }
 
-measurements["Rldr1"] <- lapply(measurements["voltage ldr1"], convert_Ohm)
-measurements["Rldr2"] <- lapply(measurements["voltage ldr2"], convert_Ohm)
+measurements["Rldr1"] <- lapply(measurements["voltage-ldr1"], convert_Ohm)
+measurements["Rldr2"] <- lapply(measurements["voltage-ldr2"], convert_Ohm)
 # values in Ohm
 
 print(measurements)
---
+++ Week3/sketch_oct11a/sketch_oct11a.ino
@@ -0,0 +1,42 @@
+// Le tensao dos LDR em mV
+const int sensorPin1 = 0;
+const int ledPin = 3;
+
+int sensorValue1 = 0;
+
+int v1;
+
+int led_value = 0;
+
+char str[128];
+
+int i=0;
+
+void setup(){
+
+  Serial.begin(9600);
+}
+
+
+
+void loop(){
+
+  sensorValue1 = analogRead(sensorPin1);
+  
+  v1 = sensorValue1*5.0/1.0230;
+  
+  Serial.println(v1);
+
+  if(i>=50){
+    led_value +=50;
+    Serial.println("Step");
+    i=0;   
+  }
+  if(led_value >= 250){
+    led_value=0;  
+  }
+  analogWrite(ledPin, led_value);
+
+  i++;
+  delay(100);
+}
