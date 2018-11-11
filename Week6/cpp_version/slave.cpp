9e9dbc96fba2450d00e810736fb52802da4d4c74
jomigo96 <jomigo96@yahoo.com>
2018-11-11 20:35:17 +0000

    Added c++ code
    Falta testar com os arduinos
    Tambem configurei o git no raspberry, estou a fazer o commit daÃ. SónÃo sei como dar net a partir do portÃtil, tenho usado wifi.

---
+++ Week6/cpp_version/slave.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <pigpio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <linux/i2c-dev.h>
+#include <stdlib.h>
+#define SLAVE_ADDR 0x48
+
+int init_slave(bsc_xfer_t &xfer, int addr){
+
+	gpioSetMode(18, PI_ALT3);
+	gpioSetMode(19, PI_ALT3);
+	xfer.control = (addr<<16) |
+			(0x00<<13) |
+			(0x00<<12) |
+			(0x00<<11) |
+			(0x00<<10) |
+			(0x01<<9) |
+			(0x01<<8) |
+			(0x00<<7) |
+			(0x00<<6) |
+			(0x00<<5) |
+			(0x00<<4) |
+			(0x00<<3) |
+			(0x01<<2) |
+			(0x00<<1) |
+			0x01;
+	return bscXfer(&xfer);
+}
+
+int main(void){
+
+	bsc_xfer_t xfer;
+
+	if(gpioInitialise() < 0){
+		std::cout << "Error 1" << std::endl;
+		return 1;
+	}
+
+	int status;
+	status = init_slave(xfer, SLAVE_ADDR);
+
+	std::cout << "Started program with status " << status << std::endl;
+
+	while(1){
+		xfer.txCnt = 0;
+		status = bscXfer(&xfer);
+		if(!(status & 1<<1)){ //checks if receive buffer is not empty
+			std::cout << "Received "<< xfer.rxCnt << " bytes" << std::endl;
+			for(int j = 0; j < xfer.rxBuf[j]; j++)
+				std::cout << xfer.rxBuf[j];
+		}
+	}
+}
